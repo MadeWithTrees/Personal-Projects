a328dd0083c586c69f874033aa39adbc2c7e0b52
Dmitri Lopez <dmitri0117@gmail.com>
2022-04-20 01:18:10 -0700

    first commit

---
+++ nierStartupScreen/nierStartup.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+bool option1;
+bool option2;
+bool option3;
+
+void checkOption(int n){
+    if (n == 1){
+
+    cout << "Nothingness. Is there any meaning to living in this world?" << endl;
+    cout << " • Pray to God. " << endl;
+    cout << " • Hold true to your own will. " << endl;
+    cout << " • I don't care. I don't need any of this! " << endl;
+}
+
+    if (option1 = 1){
+
+    cout << "God. How did God create us?" << endl;
+    cout << " • By random chance. " << endl;
+    cout << " • From nothingness. " << endl;
+    cout << " • I don't care. I don't need any of this! " << endl;
+}
+
+    if(option1 = 1){
+    
+    cout << "Chance. Was this world created by random chance? " << endl;
+    cout << " • All is according to God's will. " << endl;
+    cout << " • It was not random. This world is filled with nothingness. " << endl;
+    cout << " • I don't care. I don't need any of this! " << endl;
+    }
+}
+
+int main(){
+
+    cout << "Will. From where does our will come? " << endl;
+    cout << " • It is born from nothingness. " << endl;
+    cout << " • It is given to us by God. " << endl;
+    cout << " • I don't care. I don't need any of this! " << endl;
+    return 0;
+
+}
+
